061d0016f3f4cd134a35e9197e7fe4b7493cbb2a
MMostella <masonmostella@gmail.com>
2021-09-13 21:40:25 -0500

    Setting Up

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,5 +1,61 @@
+import React from "react";
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 
+import Display from './../Display';
 
+import mockFetchShow from './../../api/fetchShow';
+jest.mock('./../../api/fetchShow');
+
+const testShow = {
+    name: 'test show',
+    summary: 'test summary',
+    seasons: [
+        {
+            id: 0,
+            name: 'Season 1',
+            episodes: []
+        },
+        {
+            id: 1,
+            name: 'Season 2',
+            episodes: []
+        }
+    ]
+}
+
+test('Remders without errors', () => {
+    render(<Display />);
+});
+
+test('Renders show component when the button is clicked', async () => {
+    mockFetchShow.mockResolvedValueOnce(testShow);
+
+    render(<Display />);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    const show = await screen.findByTestId('show-container');
+    expect(show).toBeInTheDocument();
+});
+
+test('Renders season options matching fetch return when button is clicked', async () => {
+    mockFetchShow.mockResolvedValueOnce(testShow);
+
+    render(<Display />);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    await waitFor(() => {
+        const seasonOptions = screen.queryAllByTestId('season-option');
+        expect(seasonOptions).toHaveLength(2);
+    })
+
+});
+
+test('', () => {
+    render(<Display />);
+});
 
 
 
